7aa3bac8a288e51c63bc977ed84c8d41aa6a76a0
Aaron Roney <twitchax@gmail.com>
2025-01-08 16:37:50 -0800

    Refactor some mode calculations.

--- src/mode.rs
+++ src/mode.rs
@@ -5,44 +5,69 @@ pub fn mode_from_counts(counts: &[usize]) -> Num {
 }
 
 pub fn top_two_modes_from_counts(counts: &[usize]) -> (Num, Num) {
-    let length = counts.len();
-    let mut counts = counts.iter().enumerate().collect::<Vec<_>>();
-    counts.sort_by_key(|&(_, &count)| count);
+    let (mut first_index, mut second_index) = (0, 0);
+    let (mut first, mut second) = (counts[0], 0);
 
-    let (first, second) = (counts[length - 1].0 + 1, counts[length - 2].0 + 1);
+    for (i, &count) in counts.iter().enumerate().skip(1) {
+        if count > first {
+            second = first;
+            second_index = first_index;
+            first = count;
+            first_index = i;
+        } else if count > second {
+            second = count;
+            second_index = i;
+        }
+    }
 
-    (first, second)
+    (first_index as Num + 1, second_index as Num + 1)
 }
 
 pub fn anti_modes(counts: &[Num]) -> Vec<Num> {
-    let mode = mode_from_counts(counts);
-    let mode_count = counts[mode - 1];
-    let mut counts = counts.iter().enumerate().collect::<Vec<_>>();
+    let mode_index = mode_from_counts(counts);
+    let mode_count = counts[mode_index - 1];
 
-    counts.sort_by_key(|&(_, &count)| count);
+    // Collect min nonzero count
+    let mut min_nonzero = usize::MAX;
+    let mut nonzero_count = 0;
+    let mut mode_count_occurrences = 0;
+    for &val in counts.iter().filter(|v| **v > 0) {
+        nonzero_count += 1;
+        if val < min_nonzero {
+            min_nonzero = val;
+        }
+        if val == mode_count {
+            mode_count_occurrences += 1;
+        }
+    }
 
-    let modes = counts.iter().filter(|(_, count)| **count == mode_count).collect::<Vec<_>>();
-    let nonzeroes = counts.iter().filter(|(_, count)| **count != 0).collect::<Vec<_>>();
-
-    // There is only one nonzero count, so there are no antimodes.
-    if nonzeroes.len() <= 1 {
+    // If we have only one nonzero, then there are no antimodes.
+    if nonzero_count <= 1 {
         return vec![];
     }
-
-    // If all of the modes are the nonzeroes, then we have to choose one so it will be re-rolled.
-    if modes.len() == nonzeroes.len() {
-        return vec![nonzeroes[0].0 + 1];
+    
+    // If all nonzeroes are modes, then choose the first one to be an antinode so that the simulation can progress.
+    if mode_count_occurrences == nonzero_count {
+        let first_nonzero_index = counts.iter().position(|&v| v > 0).unwrap();
+        return vec![first_nonzero_index + 1];
     }
 
-    let anti_modes = nonzeroes.iter().take_while(|&(_, &count)| count == *nonzeroes[0].1)
-        .map(|&(index, _)| *index + 1)
-        .collect::<Vec<_>>();
-
-    anti_modes
+    // Gather antimodes with one pass
+    let mut result = Vec::new();
+    for (k, &val) in counts.iter().enumerate() {
+        if val == min_nonzero {
+            result.push(k + 1);
+        }
+    }
+    result
 }
 
 #[cfg(test)]
 mod tests {
+    use std::hint::black_box;
+
+    use crate::rand::roll;
+
     use super::*;
     use pretty_assertions::assert_eq;
 
@@ -59,7 +84,7 @@ mod tests {
     #[test]
     fn test_top_two_modes_from_counts() {
         let counts = vec![1, 2, 3, 4, 2, 3, 1, 1];
-        let expected = (4, 6);
+        let expected = (4, 3);
 
         let result = top_two_modes_from_counts(&counts);
 
@@ -94,5 +119,38 @@ mod tests {
         let result = anti_modes(&counts);
 
         assert_eq!(result, expected);
+    }
+
+    #[bench]
+    fn bench_mode_from_counts(b: &mut test::Bencher) {
+        let  size = 1_000;
+        let mut counts = Vec::with_capacity(size);
+        for _ in 0..size {
+            counts.push(roll(20));
+        }
+
+        b.iter(|| black_box(mode_from_counts(&counts)));
+    }
+
+    #[bench]
+    fn bench_top_two_modes_from_counts(b: &mut test::Bencher) {
+        let  size = 1_000;
+        let mut counts = Vec::with_capacity(size);
+        for _ in 0..size {
+            counts.push(roll(20));
+        }
+
+        b.iter(|| black_box(top_two_modes_from_counts(&counts)));
+    }
+
+    #[bench]
+    fn bench_anti_modes(b: &mut test::Bencher) {
+        let  size = 1_000;
+        let mut counts = Vec::with_capacity(size);
+        for _ in 0..size {
+            counts.push(roll(20));
+        }
+
+        b.iter(|| black_box(anti_modes(&counts)));
     }
 }
